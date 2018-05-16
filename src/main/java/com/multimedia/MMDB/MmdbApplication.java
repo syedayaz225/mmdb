6cf80b4a666181e039837e45b904fb7bb328462c
Syed Ayaz <syedayaz225@gmail.com>
2018-05-16 21:44:48 +0200

    initial commit

---
+++ src/main/java/com/multimedia/MMDB/MmdbApplication.java
@@ -0,0 +1,12 @@
+package com.multimedia.MMDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmdbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/multimedia/MMDB/MmdbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.multimedia.MMDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MmdbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
